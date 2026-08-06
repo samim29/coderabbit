4f338e9da75190bbb422a6313cbfa49bddd70299
Samim29 <samimalisk000@gmail.com>
2026-08-06 16:08:51 +0530

    updated the port from hardcoded to env

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,12 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
